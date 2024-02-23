afc93f12a7633ef27910c19b366d9e68f4d135e9
Andres Felipe Escobar <anfelesvillada@gmail.com>
2024-02-23 11:39:11 -0500

    Base y servicio causal
    Se definen las migraciones, seeders, factories y modelos. Se crea controlador de causal y su ruta api respectiva. Se monta archivo de idioma español

--- backend/orderapi/routes/api.php
+++ backend/orderapi/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CausalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/* Route::prefix('causal')->group(function () {
+    Route::controller(CausalController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('/', 'store');
+        Route::get("/{causal}", "show");
+        Route::put('/{causal}', 'update');
+        Route::delete("/{causal}", "destroy");
+    });
+});  */
+
+Route::apiResource('causal', CausalController::class);
